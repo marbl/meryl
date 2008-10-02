7550f31aee79f35f57e767837ab77a079fbac601
Brian P. Walenz <thebri@gmail.com>
2008-10-02 16:23:20 +0000

    Update to the new libseq implementaitoon in kmer r1767 and later.

--- src/meryl/meryl.C
+++ src/meryl/meryl.C
@@ -1,3 +1,26 @@
+
+/**************************************************************************
+ * This file is part of Celera Assembler, a software program that
+ * assembles whole-genome shotgun reads into contigs and scaffolds.
+ * Copyright (C) 2003-2004, Applera Corporation. All rights reserved.
+ * Copyright (C) 2004,      Brian Walenz
+ * Copyright (C) 2005-2007, J. Craig Venter Institute
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received (LICENSE.txt) a copy of the GNU General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -6,9 +29,18 @@
 #include "bio++.H"
 #include "meryl.H"
 
+#include "AS_MER_gkpStore_to_FastABase.H"
+
+//
+//  This is a SHADOW COPY!  The main really exists in kmer/meryl/meryl.C!
+//
+
 int
 main(int argc, char **argv) {
   merylArgs   *args = new merylArgs(argc, argv);
+
+  //seqFactory::instance()->registerFile(new gkpStoreFile());
+  gkpStoreFile::registerFile();
 
   switch (args->personality) {
     case 'P':
@@ -19,18 +51,18 @@ main(int argc, char **argv) {
       build(args);
       break;
 
-    case 'd':
-      dumpDistanceBetweenMers(args);
-      break;
     case 't':
       dumpThreshold(args);
-      break;
-    case 'p':
-      dumpPositions(args);
       break;
     case 'c':
       countUnique(args);
       break;
+#ifdef PLOT_DISTANCE
+      //  Can't do this with big mers
+    case 'p':
+      plotDistanceBetweenMers(args);
+      break;
+#endif
     case 'h':
       plotHistogram(args);
       break;
@@ -65,6 +97,10 @@ main(int argc, char **argv) {
   }
 
   delete args;
+
+#ifdef MEMORY_DEBUG
+  _dump_allocated_delta(fileno(stdout));
+#endif
 
   return(0);
 }
