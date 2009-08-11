5a1a8603b904719f8498c87e8fb9184106fdcbbc
Brian P. Walenz <thebri@gmail.com>
2009-08-11 04:48:32 +0000

    Add MAXEXIST, update to latest version from kmer.

--- src/meryl/meryl.C
+++ src/meryl/meryl.C
@@ -1,3 +1,28 @@
+
+/**************************************************************************
+ * This file is part of Celera Assembler, a software program that
+ * assembles whole-genome shotgun reads into contigs and scaffolds.
+ * Copyright (C) 2003-2004, Applera Corporation. All rights reserved.
+ * Copyright (C) 2004,      Brian Walenz
+ * Copyright (C) 2005-2007, J. Craig Venter Institute
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received (LICENSE.txt) a copy of the GNU General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *************************************************************************/
+
+const char *mainid = "$Id: meryl.C,v 1.7 2009-08-11 04:48:32 brianwalenz Exp $";
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -6,9 +31,23 @@
 #include "bio++.H"
 #include "meryl.H"
 
+#include "AS_MER_gkpStore_to_FastABase.H"
+#include "AS_MER_gkpStoreChain.H"
+
+#include "AS_global.h"
+
+//
+//  This is a SHADOW COPY!  The main really exists in kmer/meryl/meryl.C!
+//
+
 int
 main(int argc, char **argv) {
+  argc = AS_configure(argc, argv);
+
   merylArgs   *args = new merylArgs(argc, argv);
+
+  gkpStoreFile::registerFile();
+  gkpStoreChain::registerFile();
 
   switch (args->personality) {
     case 'P':
