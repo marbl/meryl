c951f1eb62c4b5cbbd08d543787306036254f30c
arangrhie <arrhie@gmail.com>
2019-10-15 16:44:39 -0400

    add assembly evaluation package

---
+++ scripts/_submit_cn.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+if [[ "$#" -lt 3 ]]; then
+	echo "Usage: ./_submit_cn.sh <read-db.meryl> <k> <asm1.fasta> [<asm2.fasta> <out>]"
+	exit -1
+fi
+
+readdb=$1
+k=$2
+asm1=$3
+asm2=$4
+out=$5
+
+args="$readdb $k $asm1 $asm2 $out"
+if [ -z $out ]; then
+	out="meryl"
+	args="$readdb $k $asm1"
+fi
+
+cpus=24
+mem=48g
+name=$out.spectra-cn
+script="$tools/meryl/scripts/spectra-cn.sh"
+partition=quick
+walltime=3:00:00
+path=`pwd`
+
+mkdir -p logs
+log=logs/$name.%A.log
+
+echo "\
+sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args"
+sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args 
---
+++ scripts/plot/plot_spectra_hap.R
@@ -0,0 +1,46 @@
+#!/usr/bin/env Rscript
+
+require("argparse")
+require("ggplot2")
+require("scales")
+
+parser <- ArgumentParser(description = "Make spectra-hap plots. Line and filled pectra-hap plots will be generated.")
+parser$add_argument("-f", "--file", type="character", help=".spectra-hap.hist file (required)", default=NULL)
+parser$add_argument("-o", "--output", type="character", help="output prefix (required)")
+parser$add_argument("-x", "--xdim", type="double", default=6, help="width of plot [default %(default)s]")
+parser$add_argument("-y", "--ydim", type="double", default=3, help="height of plot [default %(default)s]")
+parser$add_argument("-m", "--max", type="integer", default=100, help="maximum limit for k-mer multiplicity [default %(default)s]")
+args <- parser$parse_args()
+
+spectra_hap_plot  <-  function(hist, name, w=6, h=3, x_max=100) {
+  
+  dat=read.table(hist, header = TRUE)
+  y_max=max(dat[dat[,1]!="read-total",]$Count)
+  y_max=y_max*1.5
+  print(paste("y_max:", y_max, sep=" "))
+
+  dat[,1]=factor(dat[,1], levels=unique(dat[,1]), ordered=TRUE) # Lock in the order  
+  
+  ## Line graph
+  ggplot(data=dat, aes(x=kmer_multiplicity, y=Count, group=Assembly, colour=Assembly)) +
+    geom_line() +
+    theme_bw() +
+    scale_color_brewer(palette = "Set1") +
+    scale_y_continuous(labels=comma) +
+    coord_cartesian(xlim=c(0,x_max), ylim=c(0,y_max))
+  ggsave(file = paste(name, 'hap.ln.png', sep = "."), height = h, width = w)
+  ggsave(file = paste(name, 'hap.ln.pdf', sep = "."), height = h, width = w)
+  
+  ## Area under the curve filled
+  ggplot(data=dat, aes(x=kmer_multiplicity, y=Count)) +
+    geom_ribbon(aes(ymin=0, ymax=pmax(Count,0), fill=Assembly, colour=Assembly), alpha=0.5, linetype=1) +
+    theme_bw() +
+    scale_color_brewer(palette = "Set1", direction=1) +  # , breaks=rev(levels(dat$Assembly))
+    scale_fill_brewer(palette = "Set1", direction=1) +   # , breaks=rev(levels(dat$Assembly))
+    scale_y_continuous(labels=comma) +
+    coord_cartesian(xlim=c(0,x_max), ylim=c(0,y_max))
+  ggsave(file = paste(name, 'hap.fl.png', sep = "."), height = h, width = w)
+  ggsave(file = paste(name, 'hap.fl.pdf', sep = "."), height = h, width = w)
+}
+
+spectra_hap_plot(args$file, args$output, args$xdim, args$ydim, args$max)
