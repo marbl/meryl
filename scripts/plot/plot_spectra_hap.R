9544f1a50ec1958d7b3b67b0ac9bfebe5e068105
arangrhie <arrhie@gmail.com>
2019-10-25 09:53:54 -0400

    clean up spectra-cn

--- scripts/plot/plot_spectra_hap.R
+++ scripts/plot/plot_spectra_hap.R
@@ -9,36 +9,38 @@ parser$add_argument("-f", "--file", type="character", help=".spectra-hap.hist fi
 parser$add_argument("-o", "--output", type="character", help="output prefix (required)")
 parser$add_argument("-x", "--xdim", type="double", default=6, help="width of plot [default %(default)s]")
 parser$add_argument("-y", "--ydim", type="double", default=3, help="height of plot [default %(default)s]")
-parser$add_argument("-m", "--max", type="integer", default=100, help="maximum limit for k-mer multiplicity [default %(default)s]")
+parser$add_argument("-m", "--max", type="integer", default=150, help="maximum limit for k-mer multiplicity [default %(default)s]")
 args <- parser$parse_args()
 
-spectra_hap_plot  <-  function(hist, name, w=6, h=3, x_max=100) {
+spectra_hap_plot  <-  function(hist, name, w=6, h=3, x_max=150) {
   
   dat=read.table(hist, header = TRUE)
-  y_max=max(dat[dat[,1]!="read-total",]$Count)
+  y_max=max(dat[dat[,1]!="read-total" & dat[,1]!="read-only",]$Count)
   y_max=y_max*1.5
   print(paste("y_max:", y_max, sep=" "))
 
   dat[,1]=factor(dat[,1], levels=unique(dat[,1]), ordered=TRUE) # Lock in the order  
   
   ## Line graph
-  ggplot(data=dat, aes(x=kmer_multiplicity, y=Count, group=Assembly, colour=Assembly)) +
+  ggplot(data=dat, aes(x=kmer_multiplicity, y=Count, group=dat[,1], colour=dat[,1])) +
     geom_line() +
     theme_bw() +
     scale_color_brewer(palette = "Set1") +
     scale_y_continuous(labels=comma) +
-    coord_cartesian(xlim=c(0,x_max), ylim=c(0,y_max))
+    coord_cartesian(xlim=c(0,x_max), ylim=c(0,y_max)) +
+    labs(fill="k-mer", colour="k-mer")
   ggsave(file = paste(name, 'hap.ln.png', sep = "."), height = h, width = w)
   ggsave(file = paste(name, 'hap.ln.pdf', sep = "."), height = h, width = w)
   
   ## Area under the curve filled
   ggplot(data=dat, aes(x=kmer_multiplicity, y=Count)) +
-    geom_ribbon(aes(ymin=0, ymax=pmax(Count,0), fill=Assembly, colour=Assembly), alpha=0.5, linetype=1) +
+    geom_ribbon(aes(ymin=0, ymax=pmax(Count,0), fill=dat[,1], colour=dat[,1]), alpha=0.5, linetype=1) +
     theme_bw() +
     scale_color_brewer(palette = "Set1", direction=1) +  # , breaks=rev(levels(dat$Assembly))
     scale_fill_brewer(palette = "Set1", direction=1) +   # , breaks=rev(levels(dat$Assembly))
     scale_y_continuous(labels=comma) +
-    coord_cartesian(xlim=c(0,x_max), ylim=c(0,y_max))
+    coord_cartesian(xlim=c(0,x_max), ylim=c(0,y_max)) +
+    labs(fill="k-mer", colour="k-mer")
   ggsave(file = paste(name, 'hap.fl.png', sep = "."), height = h, width = w)
   ggsave(file = paste(name, 'hap.fl.pdf', sep = "."), height = h, width = w)
 }
--- scripts/spectra-cn.sh
+++ scripts/spectra-cn.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Usage: ./spectra-cn.sh <read.meryl> <k> <asm1.fasta> [asm2.fasta out-prefix]"
+echo "Usage: ./spectra-cn.sh <read.meryl> <k> <asm1.fasta> [asm2.fasta] out-prefix"
 echo -e "\t<read.meryl>\t: Generated with meryl count from i.e. illumina wgs reads"
 echo -e "\t<k>\t: k-size used to build <read.meryl>"
 echo -e "\t<asm1.fasta>\t: haplotype 1 assembly. gzipped or not"
 echo -e "\t[asm2.fasta]\t: haplotype 2 assembly. gzipped or not"
-echo -e "\t[out-prefix]: output prefix. Only used when asm2.fasta is provided."
+echo -e "\t<out-prefix>: output prefix. Required."
 echo
 echo -e "<asm1.spectra-cn> will be generated as output."
 echo -e "\tWhen asm2 and out-prefix are given,"
@@ -25,6 +25,7 @@ if [ -z $scripts ]; then
     scripts=`which meryl`
     scripts=`dirname $scripts`
     scripts="$scripts/../../scripts"
+    echo "Done!"
 fi
 
 read=$1
@@ -33,12 +34,20 @@ asm1_fa=$3
 asm2_fa=$4
 name=$5
 if [ -z $name ]; then
-	name="meryl"
+	name=$4
+	asm2_fa=""
+fi
+
+if [ -e $name ]; then
+	echo "$name already exists. Provide a different name."
+	exit -1
 fi
 
 ## Remove this line if R is properly installed ##
 module load R					#
 #################################################
+
+asm1=`echo $asm2_fa | sed 's/.fasta$//g' | sed 's/.fa$//g' | sed 's/.fasta.gz$//g' | sed 's/.fa.gz$//g'`
 
 echo "Generate spectra-cn.hist per assemblies"
 for asm_fa in $asm1_fa $asm2_fa	# will generate only for asm1_fa if asm2_fa is empty
@@ -47,30 +56,42 @@ do
 
 	# Generate meryl db per assembly
 	if [ ! -e $asm.meryl ]; then
-	    meryl count k=$k output ${asm}.meryl $asm_fa
+	    echo
+	    echo "Generate meryl db for $asm"
+	    meryl count memory=31 k=$k output ${asm}.meryl $asm_fa
 	fi
 
-	# Collect read counts per asm copies
+	echo
+	echo "Collect read counts per asm copies"
 	hist=asm.$asm.spectra-cn.hist
 	echo -e "Copies\tkmer_multiplicity\tCount" > $hist
+	echo "
+	meryl difference output read.k$k.$asm.0.meryl $read $asm.meryl"
 	meryl difference output read.k$k.$asm.0.meryl $read $asm.meryl
 	meryl histogram read.k$k.$asm.0.meryl | awk '{print "read-only\t"$0}' >> $hist
-	rm -r read.k$k.$asm.0.meryl
 
 	for i in $(seq 1 4)
 	do
+	    echo "
+	    meryl intersect output read.k$k.$asm.$i.meryl $read [ equal-to $i ${asm}.meryl ]"
 	    meryl intersect output read.k$k.$asm.$i.meryl $read [ equal-to $i ${asm}.meryl ]
 	    meryl histogram read.k$k.$asm.$i.meryl | awk -v cn=$i '{print cn"\t"$0}' >> $hist
 	    rm -r read.k$k.$asm.$i.meryl
 	done
+	echo "
+	meryl intersect output read.k$k.$asm.gt$i.meryl $read [ greater-than $i ${asm}.meryl ]"
 	meryl intersect output read.k$k.$asm.gt$i.meryl $read [ greater-than $i ${asm}.meryl ]
 	meryl histogram read.k$k.$asm.gt$i.meryl | awk -v cn=">$i" '{print cn"\t"$0}' >> $hist
 	rm -r read.k$k.$asm.gt$i.meryl
 
+	echo "
+	meryl difference output $asm.0.meryl ${asm}.meryl $read"
 	meryl difference output $asm.0.meryl ${asm}.meryl $read
 	meryl statistics ${asm}.0.meryl  | head -n4 | tail -n1 | awk -v asm=$asm '{print asm"\t"$1"\t"$2}' >> $name.asmonly.txt
 
 	echo "Plot $hist"
+	echo "
+	$scripts/plot/plot_spectra_cn.R -f $hist -o asm.$asm"
 	$scripts/plot/plot_spectra_cn.R -f $hist -o asm.$asm
 
 	ASM_ONLY=`meryl statistics ${asm}.0.meryl  | head -n4 | tail -n1 | awk '{print $2}'`
@@ -78,62 +99,97 @@ do
         ERROR=`echo "$ASM_ONLY $TOTAL" | awk -v k=$k '{print (1-(1-$1/$2)^(1/k))}'`
         QV=`echo "$ASM_ONLY $TOTAL" | awk -v k=$k '{print (-10*log(1-(1-$1/$2)^(1/k))/log(10))}'`
         echo -e "$asm\t$ASM_ONLY\t$TOTAL\t$QV\t$ERROR" >> $name.qv
-        rm -r ${asm}.0.meryl
 done
 
 if [ -z $asm2_fa ]; then
 	echo "No asm2_fa given. Done."
+
+	hist=$name.spectra-asm.hist
+
+	# $asm1 only
+	meryl intersect output read.k$k.$asm1.meryl $read ${asm1}.meryl
+
+	# Write output
+	echo -e "Assembly\tkmer_multiplicity\tCount" > $hist
+	meryl histogram read.k$k.$asm1.0.meryl | awk '{print "read-only\t"$0}' >> $hist
+	meryl histogram read.k$k.$asm1.meryl | awk -v hap="$asm1-only" '{print hap"\t"$0}' >> $hist
+
+	echo "
+	Plot $hist"
+	echo "
+	$scripts/plot/plot_spectra_hap.R -f $hist -o $name"
+	$scripts/plot/plot_spectra_hap.R -f $hist -o $name
+
+	echo "Clean up"
+	rm -r ${asm1}.0.meryl read.k$k.$asm1.0.meryl read.k$k.$asm1.meryl
+
+
 	exit 0
 fi
 
+rm -r read.k$k.$asm2.0.meryl
+
+echo
 echo "Generate spectra-cn.hist for combined $asm1_fa and $asm2_fa"
-echo "Is my diploid assembly having k-mers in expected copy numbers?"
+echo "\"Is my diploid assembly having k-mers in expected copy numbers?\""
 hist=$name.spectra-cn.hist
 
-
-asm1=`echo $asm2_fa | sed 's/.fasta$//g' | sed 's/.fa$//g' | sed 's/.fasta.gz$//g' | sed 's/.fa.gz$//g'`
 asm2=`echo $asm1_fa | sed 's/.fasta$//g' | sed 's/.fa$//g' | sed 's/.fasta.gz$//g' | sed 's/.fa.gz$//g'`
 
-# union-sum: ${asm1} + ${asm2} + shared kmer counts (asm)
+echo "
+Union-sum: ${asm1} + ${asm2} + shared kmer counts (asm)"
 meryl union-sum output ${asm1}_${asm2}_union.meryl ${asm1}.meryl ${asm2}.meryl
 
-# 0-counts in the asm; only seen in the read reads
+echo "
+0-counts in the asm; only seen in the reads"
 meryl difference output read.k$k.0.meryl $read ${asm1}_${asm2}_union.meryl
+echo
 
 echo -e "Copies\tkmer_multiplicity\tCount" > $hist
 meryl histogram read.k$k.0.meryl | awk '{print "read-only\t"$0}' >> $hist
 for i in $(seq 1 4)
 do
+    echo "
+    meryl intersect output read.k$k.$i.meryl $read [ equal-to $i ${asm1}_${asm2}_union.meryl ]"
     meryl intersect output read.k$k.$i.meryl $read [ equal-to $i ${asm1}_${asm2}_union.meryl ]
     meryl histogram read.k$k.$i.meryl | awk -v cn=$i '{print cn"\t"$0}' >> $hist
     rm -r read.k$k.$i.meryl
 done
 
+echo "
+meryl intersect output read.k$k.gt$i.meryl $read [ greater-than $i ${asm1}_${asm2}_union.meryl ]"
 meryl intersect output read.k$k.gt$i.meryl $read [ greater-than $i ${asm1}_${asm2}_union.meryl ]
 meryl histogram read.k$k.gt$i.meryl | awk -v cn=">$i" '{print cn"\t"$0}' >> $hist
 rm -r read.k$k.gt$i.meryl
 
-echo "Plot $name.spectra-cn.hist"
+echo "
+Plot $name.spectra-cn.hist"
+echo "
+$scripts/plot/plot_spectra_cn.R -f $hist -o $name"
 $scripts/plot/plot_spectra_cn.R -f $hist -o $name
 
-echo "Count k-mers only seen in the assemblies, not in the reads"
-# 0-counts in the read; only seen in the assembly
+echo "
+## Count k-mers only seen in the assemblies, not in the reads ##"
+echo "
+0-counts in the read; only seen in the assembly"
 meryl difference output ${asm1}_or_${asm2}.0.meryl ${asm1}_${asm2}_union.meryl $read
 meryl intersect output ${asm1}_and_${asm2}.0.meryl $asm1.0.meryl $asm2.0.meryl
 
 meryl statistics ${asm1}_or_${asm2}.0.meryl  | head -n4 | tail -n1 | awk '{print "both\t"$1"\t"$2}' >> $name.asmonly.txt
 meryl statistics ${asm1}_and_${asm2}.0.meryl | head -n4 | tail -n1 | awk '{print "shared\t"$1"\t"$2}' >> $name.asmonly.txt
 
-echo "Get QV"
+echo "
+Get QV"
 ASM_ONLY=`meryl statistics ${asm1}_or_${asm2}.0.meryl  | head -n4 | tail -n1 | awk '{print $2}'`
 TOTAL=`meryl statistics ${asm1}_${asm2}_union.meryl | head -n4 | tail -n1 | awk '{print $2}'`
-ERROR=`echo "$ASM_ONLY $TOTAL" | awk -v k=$k '{print (1-(-1/k)*log(1-$1/$2))}'`
-QV=`echo "$ASM_ONLY $TOTAL" | awk -v k=$k '{print (-10*log((-1/k)*log(1-$1/$2))/log(10))}'`
+ERROR=`echo "$ASM_ONLY $TOTAL" | awk -v k=$k '{print (1-(1-$1/$2)^(1/k))}'`
+QV=`echo "$ASM_ONLY $TOTAL" | awk -v k=$k '{print (-10*log(1-(1-$1/$2)^(1/k))/log(10))}'`
 echo -e "Both\t$ASM_ONLY\t$TOTAL\t$QV\t$ERROR" >> $name.qv
-rm -r ${asm1}_or_${asm2}.0.meryl ${asm1}_${asm2}_union.meryl
+rm -r ${asm1}_and_${asm2}.0.meryl ${asm1}_or_${asm2}.0.meryl ${asm1}_${asm2}_union.meryl ${asm1}.0.meryl ${asm2}.0.meryl
 
-echo "Generate spectra-hap.hist for combined $asm1_fa and $asm2_fa"
-echo "Is the assembled distinct portion bigger in one of the two  assemblies?"
+echo "
+## Generate spectra-hap.hist for combined $asm1_fa and $asm2_fa ##"
+echo "\"Is the assembled distinct portion bigger in one of the two assemblies?\""
 hist=$name.spectra-asm.hist
 
 # Get ${asm1} / ${asm2} / shared kmers
@@ -157,8 +213,13 @@ meryl histogram read.k$k.$asm1.meryl | awk -v hap="$asm1-only" '{print hap"\t"$0
 meryl histogram read.k$k.$asm2.meryl | awk -v hap="$asm2-only" '{print hap"\t"$0}' >> $hist
 meryl histogram read.k$k.shrd.meryl | awk -v hap="shared" '{print hap"\t"$0}' >> $hist
 
-echo "Plot $hist"
+echo "
+Plot $hist"
+echo "
+$scripts/plot/plot_spectra_hap.R -f $hist -o $name"
 $scripts/plot/plot_spectra_hap.R -f $hist -o $name
 
+echo "
+Clean up"
 rm -r read.k$k.0.meryl read.k$k.$asm1.meryl read.k$k.$asm2.meryl read.k$k.shrd.meryl ${asm1}_only.meryl ${asm2}_only.meryl ${asm1}_shrd.meryl
 
